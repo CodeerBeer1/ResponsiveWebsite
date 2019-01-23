6b1f56e5814ea559e1062a8a66c9d646c4d5848e
Khizer Butt <99053182@mydavinci.nl>
2019-01-23 13:06:09 +0100

    .

---
+++ responsive.js
@@ -0,0 +1,14 @@
+var more=document.getElementById("more");
+var navbar=document.getElementById("navbar");
+
+function moreFun() {
+
+    if (navbar.className=="hide") {
+        navbar.className="show";
+    }
+
+    else {
+        navbar.className="hide";
+    }
+
+}
